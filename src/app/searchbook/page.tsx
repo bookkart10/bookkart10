fb0d6fef88cdc65975e4e76bc9aecd8bedbf0f5e
= <=>
2023-06-23 06:53:59 +0530

    add search book

--- src/app/searchbook/page.tsx
+++ src/app/searchbook/page.tsx
@@ -1,21 +1,26 @@
 import BookComponent from "@/components/BookComponent";
+import { useSearchParams } from "next/navigation";
+import { useAppSelector } from "../../../store";
+import { BooksSelector } from "../../../store/books.slice";
 export default function SearchBookPage() {
+  const query = useSearchParams().get("q");
+  const books = useAppSelector((state) =>
+    BooksSelector.selectAll(state).filter((book) => book.book_name == query)
+  );
+
   return (
     <div>
       <div className="p-10">
         <h1 className="font-semibold text-2xl ">
-          Search result for book series
+          Search result for <b>{query}</b>
         </h1>
       </div>
       <div className="grid grid-cols-5 gap-5 px-10 pb-10">
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
-        <BookComponent />
+        {books.length > 0 ? (
+          books.map((book) => <BookComponent key={book.book_id} feed={book} />)
+        ) : (
+          <h1>No result found !</h1>
+        )}
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Searchbar from "./Searchbar";
 import Navtext from "./Navtext";
 import { MdAccountCircle } from "react-icons/md";
